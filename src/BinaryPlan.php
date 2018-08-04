7cbdb1190c7feb1678733fd93ff9fe19bd59664c
malfasih <malfasih@icloud.com>
2018-08-04 14:11:55 +0000

    adding class

---
+++ src/BinaryPlan.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Slim 3 Binary Plan - An essentials tools for creating Binary MLM System.
+ * 
+ * @package Slim
+ * @author Anya Gracelya Indraswari <annya@tigatangan.com>
+ * @license MIT
+ *
+ */
+
+namespace Malfasih\Slim;
+
+class BinaryPlan {
+
+	/**
+	 * Contains downline lists  
+	 *
+	 * @var array
+	 */
+
+	public $users = array();
+
+}
+
+?>
