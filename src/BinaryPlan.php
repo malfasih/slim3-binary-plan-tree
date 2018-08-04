53c28fbef5a8eecf4481f399930fdb32eaf84eeb
malfasih <malfasih@icloud.com>
2018-08-04 14:39:23 +0000

    writing codes

--- src/BinaryPlan.php
+++ src/BinaryPlan.php
@@ -21,6 +21,50 @@ class BinaryPlan {
 
 	public $users = array();
 
+	/**
+	 * Get username by user_key
+	 *
+	 * @return sql
+	 */
+
+	public function getUsername($user_key)
+	{
+
+	}
+
+	/**
+	 * Get email by username
+	 *
+	 * @return sql
+	 */
+
+	public function getEmail($username)
+	{
+
+	}
+
+	/**
+	 * Get upline by username
+	 *
+	 * @return sql
+	 */
+
+	public function getUpline($username)
+	{
+
+	}
+
+	/**
+	 * Get sponsor by username
+	 *
+	 * @return sql
+	 */
+
+	public function getSponsor($username)
+	{
+
+	}
+
 }
 
 ?>
